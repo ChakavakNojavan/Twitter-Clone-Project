99e8adff6b029ec268dd67eff771d927ef0fe73f
“Chakavak <chakavak.nojavan@gmail.comuser.email chakavak.nojavan@gmail.com>
2023-03-02 09:50:06 -0500

    Bookmark+Constant

---
+++ client/src/components/Bookmarks.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+const Wrapper = styled.div`
+  margin-left: 220px;
+`;
+const Bookmarks = () => {
+  return <Wrapper>Bookmarks</Wrapper>;
+};
+
+export default Bookmarks;
---
+++ client/src/components/constants.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+  // Bright purple:
+  primary: "hsl(258deg, 100%, 50%)",
+  red: "#ff006e",
+  gray: "#fff",
+  // Add more colors as needed!
+};
